0ca3df63f6ca5f19b59f1f37763d610f52306a2d
Sparsh Sukralia <sparsh.sukralia@gmail.com>
2020-09-14 03:40:55 +0545

    useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import { MenuItem, FormControl, Select, Menu } from "@material-ui/core";
 import "./App.css";
 
 function App() {
+  const [countries, setCountries] = useState(["USA", "INDIA"]);
+
   return (
     <div className="app">
-      <h1>Covid Tracker all the way</h1>
+      <div className="app-header">
+        <h1>Covid-19 Tracker</h1>
+        <FormControl className="app-dropdown">
+          <Select variant="outlined" value="abc">
+            {/* Loop over the countries */}
+            {countries.map((country) => (
+              <MenuItem value="country">{country}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
     </div>
   );
 }
