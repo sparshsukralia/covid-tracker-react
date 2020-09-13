f59d498508eaf7d28b01194a5dae9f5140e150b0
Sparsh Sukralia <sparsh.sukralia@gmail.com>
2020-09-14 04:05:05 +0545

    useEffect and API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { MenuItem, FormControl, Select, Menu } from "@material-ui/core";
 import "./App.css";
 
 function App() {
   const [countries, setCountries] = useState(["USA", "INDIA"]);
+
+  // useEffect = Runs a function on a given condition
+  // Only if the condition is changed (or a variable is changed) useEffect will run the function
+
+  useEffect(() => {
+    // async function -> send a request, wait for it, then do something with the info
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          // Loop over the data and setting the variables to values from the data
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+        });
+    };
+  }, [countries]);
 
   return (
     <div className="app">
@@ -12,9 +30,6 @@ function App() {
         <FormControl className="app-dropdown">
           <Select variant="outlined" value="abc">
             {/* Loop over the countries */}
-            {countries.map((country) => (
-              <MenuItem value="country">{country}</MenuItem>
-            ))}
           </Select>
         </FormControl>
       </div>
