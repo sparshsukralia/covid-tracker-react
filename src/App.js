d832540a2d80f5b653e5ada55b2992e073be4acc
Sparsh Sukralia <sparsh.sukralia@gmail.com>
2020-09-14 03:10:52 +0545

    covid tracker

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Covid Tracker all the way</h1>
+    </div>
+  );
+}
+
+export default App;
