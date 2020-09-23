e4e41224bdc72a70d5f290afa77c66d583746123
Sparsh Sukralia <sparsh.sukralia@gmail.com>
2020-09-24 03:33:09 +0545

    Updates

---
+++ precache-manifest.8078ab57130021227ccee1bdaee4a627.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2c287f9b8507a01cfb12e8a864aab00",
+    "url": "/covid-tracker-react/index.html"
+  },
+  {
+    "revision": "6071c1858f20eca22d2b",
+    "url": "/covid-tracker-react/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "d1cedf32e5d1799dc421",
+    "url": "/covid-tracker-react/static/css/main.640a9f5c.chunk.css"
+  },
+  {
+    "revision": "6071c1858f20eca22d2b",
+    "url": "/covid-tracker-react/static/js/2.f05ece6f.chunk.js"
+  },
+  {
+    "revision": "ebc70c097554e9e2ef968c42fad2beba",
+    "url": "/covid-tracker-react/static/js/2.f05ece6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1cedf32e5d1799dc421",
+    "url": "/covid-tracker-react/static/js/main.28a46546.chunk.js"
+  },
+  {
+    "revision": "b2c072d971b0bdd448f3",
+    "url": "/covid-tracker-react/static/js/runtime-main.e04daffc.js"
+  }
+]);
